64585412b01822d999c10ecd5d85c73f8ff59b6e
mohit-sharma <mohit.sharma4@dotsquares.com>
2026-07-07 16:56:20 +0530

    Delete some files

--- src/components/RegisterWindow/RegisterForm.tsx
+++ src/components/RegisterWindow/RegisterForm.tsx
@@ -156,6 +156,9 @@ export default function RegisterForm() {
         <h1>Create Account</h1>
         <p>
           Join now and start chatting with your friends and family. Safe, secure, and easy to use.
+        </p>   
+        <p>
+          Join now and start chatting with your friends and family. Safe, secure, and easy to use.
         </p>       
       </div>
     </div>
